cebcc90b9bccd008cf1d459be91cbea6365c13b3
jefeg <jefeg@DESKTOP-3LN4TN9>
2024-03-11 18:34:15 -0500

    actividad 2 poo

--- src/parte1_actividad2/Ejercicio_Resueto_11.java
+++ src/parte1_actividad2/Ejercicio_Resueto_11.java
@@ -12,14 +12,7 @@ public class Ejercicio_Resueto_11 {
        num2 = nn.nextInt();
        System.out.println("Digite el tercer numero: ");
        num3 = nn.nextInt();
-       if (num1 > num2 && num1 > num3){
-           System.out.println(num1 + " es el mayor de los tres");
-       } else if (num2>num1 && num2>num3){
-           System.out.println(num2+" es el mayor de los tres");
-       }else if (num3>num2 && num3>num1){
-           System.out.println(num3 + " es el mayor de los tres");
-       }
-       
+       Numero_Mayor mayor1 = new Numero_Mayor(num1, num2, num3);              
     }
     
 }
---
+++ src/parte1_actividad2/Numero_Mayor.java
@@ -0,0 +1,14 @@
+package parte1_actividad2;
+
+public class Numero_Mayor {
+    int nume1, nume2, nume3;
+    public Numero_Mayor(int nume1, int nume2, int nume3){
+         if (nume1 > nume2 && nume1 > nume3){
+           System.out.println(nume1 + " es el mayor de los tres");
+       }else if (nume2>nume1 && nume2>nume3){
+           System.out.println(nume2+" es el mayor de los tres");
+       }else if (nume3>nume2 && nume3>nume1){
+           System.out.println(nume3 + " es el mayor de los tres");
+       }
+    }   
+}
